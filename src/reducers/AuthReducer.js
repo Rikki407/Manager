bc20324c4b065deb63ca0c902764dd8470147c61
Rikki407 <rishablamba407@gmail.com>
2018-06-25 14:03:30 +0530

    updated

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,6 +1,9 @@
+import { EMAIL_CHANGED } from '../actions/types';
+
 const INITIAL_STATE = { email: '' };
-export default (state=INITIAL_STATE, action) => {
+export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
+        case EMAIL_CHANGED:
         default:
             return state;
     }
