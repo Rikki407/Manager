3172d3d4a249a81d8d43d1363f81a2894828b6ff
Rikki407 <rishablamba407@gmail.com>
2018-06-27 23:40:17 +0530

    Navigation Enabled

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -19,7 +19,7 @@ class LoginForm extends Component {
     renderError() {
         if (this.props.error) {
             return (
-                <View style={{ backgroundColor: 'White' }}>
+                <View style={{ backgroundColor: 'white' }}>
                     <Text style={styles.errorTextStyle}>
                         {this.props.error}
                     </Text>
