15fe2011acad0f2c4ef63967efad8e8fdc2ebea8
Rikki407 <rishablamba407@gmail.com>
2018-06-28 12:00:20 +0530

    employee reducer created

---
+++ src/actions/EmployeeActions.js
@@ -0,0 +1,10 @@
+import { 
+    EMPLYEE_UPDATE 
+} from './types';
+
+export const EmployeeUpdate = ({ prop, value}) => {
+    return {
+        key: EMPLYEE_UPDATE,
+        payload: { prop, value }
+    };
+};
---
+++ src/reducers/EmpoyeeFormReducer.js
@@ -0,0 +1,10 @@
+import { EMPLYEE_UPDATE } from '../actions/types';
+
+const INITIAL_STATE = {};
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        default: 
+            return state;
+    }
+};
