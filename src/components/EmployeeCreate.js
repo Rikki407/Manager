1d0458f179f51a847dcb35a5f8cf81127046f20b
Rikki407 <rishablamba407@gmail.com>
2018-06-29 14:10:53 +0530

    create button functionality

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeCreate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreateForm extends Component {
+    onButtonPress() {
+        const { name, phone, shift } = this.props;
+        this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+    }
     render() {
         return (
             <Card>
@@ -41,7 +45,7 @@ class EmployeeCreateForm extends Component {
                 </CardSection>
                 
                 <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                         Create 
                     </Button>
                 </CardSection>
@@ -59,4 +63,6 @@ const mapStateToProps = (state) => {
     const { name, phone, shift } = state.employeeForm;
     return { name, phone, shift };
 };
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreateForm);
+export default connect(mapStateToProps, { 
+    employeeUpdate, employeeCreate 
+})(EmployeeCreateForm);
