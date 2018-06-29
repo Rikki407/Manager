11308b2c26a876a5cdd765d4848a5e2ce3477ad9
Rikki407 <rishablamba407@gmail.com>
2018-06-29 14:20:32 +0530

    connection with firebase

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 import {
     EMPLOYEE_UPDATE,
     EMPLOYEE_CREATE
@@ -11,9 +12,7 @@ export const employeeUpdate = ({ prop, value }) => {
 };
 
 export const employeeCreate = ({ name, phone, shift }) => {
-    console.log(name, phone, shift);
-    return {
-        type: EMPLOYEE_CREATE,
-        payload: { name, phone, shift }
-    };
+    const { currentUser } = firebase.auth();
+   firebase.database().ref(`/users/${currentUser.uid}/employees`)
+    .push({ name, phone, shift });
 };
