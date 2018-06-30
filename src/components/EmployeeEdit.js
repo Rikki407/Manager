3213fbe3e1b7093e0f82fee5e7253286d5bcdd80
Rikki407 <rishablamba407@gmail.com>
2018-06-30 16:46:40 +0530

    updating firebase database

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import EmployeeForm from './EmpoyeeForm';
 import { Card, CardSection, Button } from './common';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeSave } from '../actions';
 
 class EmployeeEdit extends Component {
     componentWillMount() {
@@ -14,7 +14,7 @@ class EmployeeEdit extends Component {
 
     onButtonPress() {
         const { name, phone, shift } = this.props;
-        console.log(name, phone, shift);
+        this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
     }
 
     render() {
@@ -34,4 +34,4 @@ const mapStateToProps = state => {
     const { name, phone, shift } = state.employeeForm;
     return { name, phone, shift };
 }
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);
